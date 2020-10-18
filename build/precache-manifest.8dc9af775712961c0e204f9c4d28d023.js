cc072e0de4fc7ef93cf81216ac2e8095fbc6d6a5
Abhidnyajoshi <abhidnyajoshi137@gmail.com>
2020-10-19 00:53:52 +0530

    adding build file

---
+++ build/precache-manifest.8dc9af775712961c0e204f9c4d28d023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2af177ec266bf3d48c559e8a929ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5a376a13c0b1a73616c",
+    "url": "/static/css/main.0d1f9224.chunk.css"
+  },
+  {
+    "revision": "6e5b27f28205a40e8a35",
+    "url": "/static/js/2.ddf6e74e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ddf6e74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a376a13c0b1a73616c",
+    "url": "/static/js/main.c4c94491.chunk.js"
+  },
+  {
+    "revision": "685b292dfdd8dd98b797",
+    "url": "/static/js/runtime-main.7f3d647a.js"
+  }
+]);
